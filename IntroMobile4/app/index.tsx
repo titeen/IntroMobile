065d2a701763eb3809bc69195d468b8889c0c946
Fatima Huseein <fatima.huseein@student.ap.be>
2025-02-12 14:24:23 +0100

    Start project

---
+++ IntroMobile4/app/index.tsx
@@ -0,0 +1,18 @@
+"use dom"
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+export default Index;
